8f36f4fa4136931e337776b68d939d4209de1348
LiliaCH <lilia.el44@gmail.com>
2016-05-01 19:10:56 -0500

    revision1
    uploading

---
+++ violinista.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class violinista here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class violinista extends musicos
+{
+    /**
+     * Act - do whatever the violinista wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+  
+    public void act()
+    {
+     if(Greenfoot.isKeyDown("v"))
+     {
+        super.act();
+        }
+    }
+    
+}
